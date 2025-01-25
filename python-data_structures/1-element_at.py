6e8ce72fb4f1c618be3504624846ae34f571681b
John Nkp <john.nkp1@gmail.com>
2025-01-26 02:47:07 +1100

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 # Write a function that retrieves an element from a list.
-# element_at(my_list, idx) 
+# element_at(my_list, idx)
 
 def element_at(my_list, idx):
-  if idx < 0 or idx >= len(my_list):
-    return None
-  return (my_list[idx])
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return (my_list[idx])
