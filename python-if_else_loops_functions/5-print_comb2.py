315f11c363bde19b563fc440516a2aaf15bf5c26
John Nkp <john.nkp1@gmail.com>
2025-01-25 20:26:20 +1100

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -5,9 +5,6 @@
 # The last number should be followed by a new line
 # You can only use no more than 2 print functions with string format
 # You can only use one loop in your code
-# for number in range(0, 100):
-#     if number < 99:
-#         print("{:02}, ".format(number), end="")
-#     else:
-#         print("{:02}".format(number))
+
 print(", ".join("{:02}".format(number) for number in range(100)))
+
