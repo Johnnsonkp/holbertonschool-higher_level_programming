b94a00bfa4120f72c29efa93452a3c9484b1af44
John Nkp <john.nkp1@gmail.com>
2025-01-25 21:04:42 +1100

    2-print.py

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 2-print.py
+# prints "Programming is like building a multilingual puzzle"
+# followed by a new line.
+
+print("\"Programming is like building a multilingual puzzle")
