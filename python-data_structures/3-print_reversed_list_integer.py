9a4dc7a7dcbcdf828aa8c538a081269c01a62aba
John Nkp <john.nkp1@gmail.com>
2025-01-26 16:49:02 +1100

    2-replace_in_list.py

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -7,7 +7,7 @@ def print_reversed_list_integer(my_list=[]):
     if len(my_list) > 0:
         i = len(my_list) - 1
         while i >= 0:
-            print("{}".format(my_list[i]))
+            print("{:d}".format(my_list[i]))
             i -= 1
     else:
         return
