f71f527f7b290e9d1bf73eeb496296a185234bfe
John Nkp <john.nkp1@gmail.com>
2025-01-28 05:55:40 +1100

    4-only_diff_elements.py

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,15 +4,4 @@
 
 
 def only_diff_elements(set_1, set_2):
-    new_set = set()
-
-    for i in set_1:
-        for x in set_2:
-            if i == x:
-                new_set.remove(i)
-                break
-            else:
-                new_set.add(i)
-                new_set.add(x)
-
-    return new_set
+    return set_1 ^ set_2
