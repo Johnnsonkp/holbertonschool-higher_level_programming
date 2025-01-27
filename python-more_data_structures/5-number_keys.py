c4246308290bcdfc45c488b9e913c5aadef3c56d
John Nkp <john.nkp1@gmail.com>
2025-01-28 06:00:38 +1100

    5-number_keys.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+# def number_keys(a_dictionary):
+# returns the number of keys in a dictionary
+
+
+def number_keys(a_dictionary):
+    return len(a_dictionary)
