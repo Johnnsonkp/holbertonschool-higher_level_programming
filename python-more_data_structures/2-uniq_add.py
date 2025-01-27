bea5b4b22f6144bec00f9133c678f7173fb6ce46
John Nkp <john.nkp1@gmail.com>
2025-01-28 04:19:27 +1100

    2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# adds all unique int in a list (once for each int)
+# def uniq_add(my_list=[]):
+
+
+def uniq_add(my_list=[]):
+    new_list = 0
+    for i in set(my_list):
+        new_list += i
+
+    print("{:d}".format(new_list))
