c89c6d8a8cf28ceac6b60162e3a5720b7dc62c6d
John Nkp <john.nkp1@gmail.com>
2025-01-28 16:18:38 +1100

    7-update_dictionary.py

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -8,7 +8,7 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    if len(a_dictionary) > 0:
+    if (len(a_dictionary) > 0 or len(key) > 0):
         a_dictionary[key] = value
 
     return a_dictionary
