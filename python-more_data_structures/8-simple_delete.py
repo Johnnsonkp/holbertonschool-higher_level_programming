6ff9bd87f3032c5691c995b2aada765c209531fc
John Nkp <john.nkp1@gmail.com>
2025-01-28 16:49:28 +1100

    8-simple_delete.py

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# function that deletes a key in a dictionary
+# key argument will be always a string
+# If a key doesn’t exist, the dictionary won’t change
+
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
