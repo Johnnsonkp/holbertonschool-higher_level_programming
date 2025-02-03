5e49582b70ab1533d3e372baf745159bd51c0fcf
John Nkp <john.nkp1@gmail.com>
2025-02-03 21:51:33 +1100

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,9 +6,22 @@
 #   raise a TypeError exception with the message
 #   "a must be an integer" or b must be an integer
 # a and b must be first casted to integers if they are float
-
+"""Add Integer or Float Module."""
 
 def add_integer(a, b=98):
+    """ 
+    ADD Two integers or floats a and b
+    
+    args: 
+      a (int or float): first args
+      b (int or float): first args
+
+    raises: 
+      TypeError: in case the arguments are not int or float
+
+    Return: Sum of a and b 
+    """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
