8b78f8449c30ed1b46cbd827fbba803b3556be81
John Nkp <john.nkp1@gmail.com>
2025-02-06 10:22:30 +1100

    Python - Inheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # def lookup(obj):
 """ 
-  def lookup(obj):
-  function that returns the list of available attributes 
-  and methods of an object: 
-  Returns a list object
+def lookup(obj):
+function that returns the list of available attributes 
+and methods of an object: 
+Returns a list object
 """
 
 
