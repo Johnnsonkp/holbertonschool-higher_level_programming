3bbb424be379525d47a29eb4a498b9ecac436e7c
John Nkp <john.nkp1@gmail.com>
2025-02-06 10:20:05 +1100

    Python - Inheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,11 +2,13 @@
 # def lookup(obj):
 """ 
   def lookup(obj):
-  function that returns the list of available attributes and methods of an object: 
+  function that returns the list of available attributes 
+  and methods of an object: 
   Returns a list object
 """
 
 
 def lookup(obj):
+    """returns a list of available attributes and methods of an object"""
     if isinstance(obj, int) != True:
         return dir(obj)
