f8bcdedceb5307775aa10ea5b49c498e0c045680
John Nkp <john.nkp1@gmail.com>
2025-02-06 12:15:33 +1100

    1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# def print_sorted(self):
+
+"""
+def print_sorted(self):
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
