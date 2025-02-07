30d2f470cd13d076132f35dd9abd42160dd893b8
John Nkp <john.nkp1@gmail.com>
2025-02-07 13:54:25 +1100

    Python - Inheritance

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,6 +8,19 @@ def print_sorted(self):
 
 
 class MyList(list):
+    """MyList Class that contains the print_sorted method"""
+
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
+        new_arr = self
+
+        for i in range(len(new_arr)):
+            for x in range(len(new_arr)):
+                if type(new_arr[i]) != type(new_arr[x]):
+                    raise TypeError(
+                        "'<' not supported between instances of '{}' and '{}'".format(
+                            type(new_arr[i]).__name__, type(new_arr[x]).__name__
+                        )
+                    )
+
         print(sorted(self))
