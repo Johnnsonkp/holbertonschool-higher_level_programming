68be855d2ff918568b1138b726ea8ec75f30a08e
John Nkp <john.nkp1@gmail.com>
2025-02-09 14:58:10 +1100

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry with 
+a public instance method: def area(self): 
+that raises an Exception with the message 
+area() is not implemented
+"""
+
+
+class BaseGeometry:
+    """
+    class of BaseGeometry
+    """
+
+    def area(self):
+        """
+        raises an Exception with the message
+        area() is not implemented
+        """
+        raise Exception("area() is not implemented")
