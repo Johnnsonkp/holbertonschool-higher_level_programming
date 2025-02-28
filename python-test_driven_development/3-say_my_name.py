adc5a18d81e70dd3fa7a137aeff55595908dc46b
John Nkp <john.nkp1@gmail.com>
2025-02-28 15:43:20 +1100

    3-say_my_name.py

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+  function that prints My name is <first name> <last name>
+  def say_my_name(first_name, last_name=""):
+  first_name and last_name must be string
+  or raise TypeError:
+  first_name must be a string or
+  last_name must be a string
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    if(first_name == ""):
+        raise TypeError("first_name must be present")
+    if(last_name == ""):
+        raise TypeError("last_name must be present")
+    if (isinstance(first_name, str) == False):
+        raise TypeError("first_name must be a string")
+    if (isinstance(last_name, str) == False):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
