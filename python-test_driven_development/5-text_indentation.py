45431ce270addc7cbfe30c17cf33571255146e77
John Nkp <john.nkp1@gmail.com>
2025-02-28 19:20:08 +1100

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,6 +9,7 @@ There should be no space at the beginning or at the
 end of each printed line
 """
 
+
 def text_indentation(text):
     """a function that prints a text with 2 new lines after"""
     new = ""
