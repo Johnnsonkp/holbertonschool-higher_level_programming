983e92c6270a8cd5fdc8e867eab18ba882023365
John Nkp <john.nkp1@gmail.com>
2025-03-12 00:15:38 +1100

    9-cities_by_state_join.sql

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists all the cities of California that can be found in the database `hbtn_0d_usa`
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
