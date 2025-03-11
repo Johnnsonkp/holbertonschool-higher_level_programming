725fad07ab6fdcc706b75e70eacf150e68de6b44
John Nkp <john.nkp1@gmail.com>
2025-03-12 00:57:25 +1100

    12-no_genre.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- lists all the cities of California that can be found in the database `hbtn_0d_usa`
-SELECT * 
+SELECT id, name 
 FROM cities 
 WHERE state_id = (SELECT id FROM states WHERE name = 'California') 
+AND name IN ('San Francisco', 'San Diego', 'San Jose') 
 ORDER BY id ASC;
