840de0a59b8f93df706ffbd7bd462c21bd1e1ee0
John Nkp <john.nkp1@gmail.com>
2025-03-11 23:27:45 +1100

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,9 @@
 -- creates the databses hbtn_0d_usa, the table states (in the database hbtn_0d_usa) 
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa` (
-  `id`  INT NOT NULL DEFAULT 1,
-  `name`  VARCHAR(256) NOT NULL DEFAULT 1
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`
+
+USE `hbtn_0d_usa`
+
+CREATE TABLE IF NOT EXISTS `states`(
+  `id`  INT AUTO_INCREMENT PRIMARY KEY,
+  `name`  VARCHAR(256) NOT NULL
 )
