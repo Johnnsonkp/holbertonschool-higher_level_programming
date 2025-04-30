166b967aeec20a09b890544725b0a491cb0c8191
John Nkp <john.nkp1@gmail.com>
2025-04-30 12:54:47 +1000

    minor change

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Write a function def copy_list(a_list): that returns a copy of a list.
+The input list can contain any type of objects
+Your file should be maximum 3-line long (no documentation needed)
+You are not allowed to import any module
+"""
+
+def copy_list(a_list):
+  return a_list[:]
